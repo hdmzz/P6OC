ba94a44e3a8c90e8c1a56ebfbf19507b5cc95c32
hdmzz <73470122+hdmzz@users.noreply.github.com>
2021-03-11 10:55:03 +0100

    Add files via upload

--- routes/sauce.js
+++ routes/sauce.js
@@ -7,10 +7,10 @@ const sauceCtrl = require('../controllers/sauce');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-router.get('/', auth, multer,sauceCtrl.getSauces);
-router.get('/:id', auth, multer,sauceCtrl.getOneSauce);
+router.get('/', auth, sauceCtrl.getSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-router.delete('/:id', auth, multer, sauceCtrl.deleteSauce);
-router.post('/:id/like', auth, multer, sauceCtrl.likeSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
 module.exports = router;
